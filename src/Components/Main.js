9e0fe4784e69aa6af55edd8bb66f937ef9b4918f
Alan Pieczonka <alanpieczonka@gmail.com>
2017-08-18 11:50:41 +0200

    Basic logic in components, also with CSS classes

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,8 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Square from './Square';
 
 class Main extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
     render(){
         const nextTurnVar = "X"
         const player = "{player}";
@@ -14,15 +36,15 @@ class Main extends React.Component{
                     <h4 className="h4">Next turn: <span className="span span--nextturn span--x">{nextTurnVar}</span></h4>
                 </div>
                 <div className="board">
-                    <Square />
-                    <Square />
-                    <Square />
-                    <Square />
-                    <Square />
-                    <Square />
-                    <Square />
-                    <Square />
-                    <Square />
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
                 </div>
                 <div className="row row--center">
                     <h5 className="h5 h5--win">Player {player} won</h5>
--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
+
 class Square extends Component {
+  
   render () {
     return (
-      <button className="item item1"><span className="span span--x">X</span></button>
+      <button className="item item1" onClick={() => this.props.onClick()}>
+        <span className={`span span--${this.props.value}`}>{this.props.value}</span>
+      </button>
     )
   }
 }
