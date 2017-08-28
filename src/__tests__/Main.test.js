2c4878bb9d19b087a0c26fccacd6a50f66de63d7
Alan Pieczonka <alanpieczonka@gmail.com>
2017-08-28 22:15:05 +0200

    Main.js initial tests

--- src/__tests__/Main.test.js
+++ src/__tests__/Main.test.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import sinon from 'sinon';
+
 import { expect, context } from 'chai';
 import {mount, render, shallow} from 'enzyme';
 
 import Main from './../Components/Main';
 
 describe('Main Component', () => {
-    it('ss', () => {
-        expect('h1').to.equal('h1');    
+
+    it('has state: squares which is an Array', () => {
+        const wrapper = shallow(<Main />);
+        expect(wrapper.state().squares).to.be.an('Array'); 
+    })
+
+    it('has state: squares that includes null values', () => {
+        const wrapper = shallow(<Main />);
+        expect(wrapper.state().squares).to.include(null);; 
+    })
+
+    it('has state: xIsNext, which is true', () => {
+        const wrapper = shallow(<Main />);
+        expect(wrapper.state().xIsNext).to.equal(true);
       });
 });
